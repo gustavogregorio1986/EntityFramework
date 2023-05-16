8cb293c19f90cc8199bb4d482b438b7907fdf0d5
guga56 <gustavogregorio086@gmail.com>
2023-05-16 17:14:02 -0300

    criar o repositorio e serviços do projeto

---
+++ EntityFramework/EntityFramework.Data/Repositorio/Interfce/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using EntityFramework.Dominio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data.Repositorio.Interfce
+{
+    public interface IUsuarioRepository : IGenericRepository<Usuario>
+    {
+    }
+}
---
+++ EntityFramework/EntityFramework.Servico/Servico/Interface/IUsuarioServico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Servico.Servico.Interface
+{
+    internal interface IUsuarioServico
+    {
+    }
+}
